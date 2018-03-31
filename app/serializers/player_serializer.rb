a2bf5d3f6a595ecacb7054ffc8d0b3ce2aa6ee36
Adam <lucia.adam@gmail.com>
2018-03-31 10:13:34 -0400

    refactor serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -16,17 +16,8 @@ class PlayerSerializer
   def self.mass_serialize(players)
     result_string = '['
     players.each do |player|
-      serialized_player = '{'
-      serialized_player += '"id": ' + player.id.to_s + ', '
-      serialized_player += '"name_brief": "' + player.name_brief + '", '
-      serialized_player += '"first_name": "' + player.first_name + '", '
-      serialized_player += '"last_name": "' + player.last_name + '", '
-      serialized_player += '"position": "' + player.position.title + '", '
-      player.age ? (serialized_player += '"age": ' + player.age.to_s + ', ') : (serialized_player += '"age": "null", ')
-      player.age ? (serialized_player += '"average_position_age_diff": ' + player.average_position_age_diff.to_s) : (serialized_player += '"average_position_age_diff": "null"')
-      # serialized_player += '"age": ' + player.age.to_s + ', '
-      # serialized_player += '"average_position_age_diff": ' + player.average_position_age_diff.to_s
-      serialized_player += '}, '
+      serialized_player = PlayerSerializer.serialize(player)
+      serialized_player += ', '
 
       result_string << serialized_player
     end
